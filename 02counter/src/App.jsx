0806a526fbcb380aa77e9fea294bc75a73790215
Prabhat Patel <prabhatpatel508@outlook.com>
2023-08-22 06:01:14 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App()
+{
+
+  let [counter, setCounter] = useState(15)
+
+  //let counter = 15
+
+  const addValue = () =>
+  {
+    //counter = counter + 1
+    if (counter >= 20)
+    {
+      setCounter(20);
+    } else
+    {
+      setCounter(counter + 1)
+    }
+
+  }
+
+  const removeValue = () =>
+  {
+    if (counter <= 0)
+    {
+      setCounter(0);
+    } else
+    {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+        onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+      <p>min value 0 and max value 20</p>
+    </>
+  )
+}
+
+export default App
