d25799f36d8dec955f111bebbc14a33f99e1dfa4
Prabhat Patel <prabhatpatel508@outlook.com>
2023-09-17 18:27:59 +0530

    basic routing

---
+++ 07reactRouterDom/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () =>
+{
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
---
+++ 07reactRouterDom/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const User = () =>
+{
+    const { userid } = useParams();
+    return (
+        <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
+    )
+}
+
+export default User
