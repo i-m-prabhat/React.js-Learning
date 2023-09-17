b025251dd60f85dfaa0e8dbf9af3b2d497f83130
Prabhat Patel <prabhatpatel508@outlook.com>
2023-09-17 21:28:43 +0530

    todolist with context api

---
+++ 10todoListLocalstorage/src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+// import './App.css'
+import { TodoProvider } from './contexts'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
+
+function App()
+{
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) =>
+  {
+    setTodos((prevTodo) => [...prevTodo, { id: Date.now(), ...todo }])
+  }
+
+  const updateTodo = (id, todo) =>
+  {
+    // setTodos(prevState =>
+    // {
+    //   const index = prevState.findIndex(({ id }) => id === id);
+    //   return [...prevState.slice(0, index), { ...prevState[index], ...todo }, ...prevState.slice(index + 1
+    //   )]
+    // });
+
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)));
+  }
+
+  const deleteTodo = (id) =>
+  {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  }
+
+  const toggleComplete = (id) =>
+  {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() =>
+  {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0)
+    {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() =>
+  {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/* <TodoItem /> */}
+            {
+              todos.map((todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo} />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  )
+}
+
+export default App
