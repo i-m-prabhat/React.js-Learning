640d80ecd8bc51685e9b2e63e25ca02c13022454
Prabhat Patel <prabhatpatel508@outlook.com>
2023-10-03 01:36:55 +0530

    configuring .env

---
+++ BlogApp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App()
+{
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <h1>A Blog with AppWrite</h1>
+    </>
+  )
+}
+
+export default App
